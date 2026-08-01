5345fd655d4c995913822f8aeabfa1f2c5f0d100
Julian Puga <julipuga2008@gmail.com>
2026-08-01 19:56:20 -0400

    feat: add Dockerfile and standalone output for Railway

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     domains: ['pbs.twimg.com', 'avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
   },
